1b780366d37936c5cca2874d180cbb7120694112
chilleno <antonion.gonzalezf@gmail.com>
2024-01-07 19:19:56 -0300

    feat: delete unused component

--- src/app/api/server/route.ts
+++
@@ -1,5 +0,0 @@
-const handler = async () => {
-    return new Response(JSON.stringify({ code: 420, state: 200, message: "visit /login to login with Blizzard oauth" }));
-}
-
-export { handler as GET }
