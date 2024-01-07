8353e461f72edb1223d11528b852d9bf8a12e5f4
chilleno <antonion.gonzalezf@gmail.com>
2024-01-07 20:53:40 -0300

    feat: add wow server regions

---
+++ src/app/utils/regions.ts
@@ -0,0 +1,22 @@
+export const regions = [
+    {
+        name: 'North America',
+        identifier: 'us'
+    },
+    {
+        name: 'Europe',
+        identifier: 'eu'
+    },
+    {
+        name: 'Korea',
+        identifier: 'kr'
+    },
+    {
+        name: 'Taiwan',
+        identifier: 'tw'
+    },
+    {
+        name: 'China',
+        identifier: 'cn'
+    }
+]
